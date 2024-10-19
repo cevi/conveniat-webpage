8dfbec99a3588d36c28d7545a1b266c365f6962c
Cyrill Püntener <wp99cp@gmx.net>
2024-10-19 12:11:22 +0200

    fix enable dynamic route

--- src/app/(payload)/api/auth/[...nextauth]/route.ts
+++ src/app/(payload)/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,5 @@ import { authOptions } from '@/lib/auth'
 
 const handler = NextAuth(authOptions)
 
-export const dynamic = true
+export const dynamic = 'force-dynamic'
 export { handler as GET, handler as POST }
