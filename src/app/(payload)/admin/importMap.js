42b9c65d05c572a33b0ba99a619bafdead3a03df
Cyrill Püntener <wp99cp@gmx.net>
2024-10-24 16:45:31 +0200

    merge origin/dev

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,13 +1,13 @@
-import { default as default_0 } from '@/utils/localizedCollection/components/publishingStatus'
-import { default as default_1 } from '@/utils/localizedCollection/components/autoTranslate'
-import { default as default_2 } from '@/utils/localizedCollection/components/publishLocalized'
-import { default as default_3 } from '@/components/payload/login'
-import { default as default_4 } from '@/components/payload/dashboardWelcomeBanner'
+import { default as default_7fa041d179276453b5f2f9393e9bbd6e } from '@/utils/localizedCollection/components/publishingStatus'
+import { default as default_74fb94073fb9167b06eb6b783b4ee3fa } from '@/utils/localizedCollection/components/autoTranslate'
+import { default as default_2c2463c13adff3bd1c62ce908cf29578 } from '@/utils/localizedCollection/components/publishLocalized'
+import { default as default_468452e575ad66e753c7045adde05940 } from '@/components/payload/login'
+import { default as default_dbc7e56c6c46c5d54c4604e9e4c1fb1a } from '@/components/payload/dashboardWelcomeBanner'
 
 export const importMap = {
-  "@/utils/localizedCollection/components/publishingStatus#default": default_0,
-  "@/utils/localizedCollection/components/autoTranslate#default": default_1,
-  "@/utils/localizedCollection/components/publishLocalized#default": default_2,
-  "@/components/payload/login#default": default_3,
-  "@/components/payload/dashboardWelcomeBanner#default": default_4
+  "@/utils/localizedCollection/components/publishingStatus#default": default_7fa041d179276453b5f2f9393e9bbd6e,
+  "@/utils/localizedCollection/components/autoTranslate#default": default_74fb94073fb9167b06eb6b783b4ee3fa,
+  "@/utils/localizedCollection/components/publishLocalized#default": default_2c2463c13adff3bd1c62ce908cf29578,
+  "@/components/payload/login#default": default_468452e575ad66e753c7045adde05940,
+  "@/components/payload/dashboardWelcomeBanner#default": default_dbc7e56c6c46c5d54c4604e9e4c1fb1a
 }
