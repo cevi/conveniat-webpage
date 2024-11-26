bd366771b29405921e3040ebe996b11cf597bf8d
Cyrill Püntener <wp99cp@gmx.net>
2024-11-26 13:49:17 +0100

    disable caching to prevent build errors (this needs some improvements)

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,1 +1,7 @@
 export { generateManifest as default } from '@/utils/generate-manifest';
+
+// TODO: bring back caching, e.g. to two hours
+//  currently the issue is that without 'force-dynamic' the page
+//  gets pre-rendered at build time which does not work as
+//  payload is not available at build time
+export const dynamic = 'force-dynamic';
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,7 @@
 export { generateSitemap as default } from '@/utils/generate-sitemap';
+
+// TODO: bring back caching, e.g. to two hours
+//  currently the issue is that without 'force-dynamic' the page
+//  gets pre-rendered at build time which does not work as
+//  payload is not available at build time
+export const dynamic = 'force-dynamic';
