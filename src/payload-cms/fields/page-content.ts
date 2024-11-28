d1a17f2bdaca87aec76c420afc488b4541335241
maede97 <maede97@hotmail.com>
2024-11-28 14:22:31 +0100

    Add FormBlock in Backend. - not shown in page currently

--- src/payload-cms/fields/page-content.ts
+++ src/payload-cms/fields/page-content.ts
@@ -1,6 +1,7 @@
 import { BlocksField } from 'payload';
 import { subheadingH2 } from '@/payload-cms/blocks/subheading-h2-block';
 import { paragraph } from '@/payload-cms/blocks/paragraph-block';
+import { formBlock } from '@/payload-cms/blocks/form-block';
 
 export const pageContent: BlocksField = {
   name: 'pageContent',
@@ -9,5 +10,5 @@ export const pageContent: BlocksField = {
   admin: {
     description: 'The content of the landing page',
   },
-  blocks: [subheadingH2, paragraph],
+  blocks: [subheadingH2, paragraph, formBlock],
 };
