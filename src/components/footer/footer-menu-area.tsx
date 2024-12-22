b6ce02b6a3ba4022b0b884488c2b2dfd1a6843e8
maede97 <maede97@hotmail.com>
2024-12-22 12:08:05 +0100

    add IBAN to footer, implements #42

--- src/components/footer/footer-menu-area.tsx
+++ src/components/footer/footer-menu-area.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { getPayload } from 'payload';
+import config from '@payload-config';
 
-export const FooterMenuArea: React.FC = () => {
+export const FooterMenuArea: React.FC = async () => {
+  const payload = await getPayload({ config });
+  const globalFooter = await payload.findGlobal({slug: 'footer'});
+  const IBAN = globalFooter?.donationIban;
+
   return (
     <div className="flex h-[260px] w-full flex-col items-center justify-center space-y-8 bg-green-200">
       <div className="flex flex-col items-center justify-center">
         <span className="font-heading text-[14px] font-extrabold text-green-600">Spenden</span>
         <span className="font-inter text-[14px] font-normal leading-[24px] text-green-600">
-          CH23 8080 8002 2706 7598 8
+          {IBAN}
         </span>
       </div>
 
