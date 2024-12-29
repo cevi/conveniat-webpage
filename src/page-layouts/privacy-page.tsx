f269622164d40829eb1e191752edf6de5c67324e
Cyrill Püntener <wp99cp@gmx.net>
2024-12-29 09:29:36 +0100

    make privacy-page.tsx and imprint-page.tsx configurable

--- src/page-layouts/privacy-page.tsx
+++ src/page-layouts/privacy-page.tsx
@@ -1,10 +1,23 @@
 import { LocalizedPage } from '@/page-layouts/localized-page';
 import React from 'react';
+import { getPayload } from 'payload';
+import config from '@payload-config';
+import { HeadlineH1 } from '@/components/typography/headline-h1';
+import { LexicalPageContent } from '@/components/content-blocks/lexical-page-content';
+import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical';
 
-export const PrivacyPage: React.FC<LocalizedPage> = (properties) => {
+export const PrivacyPage: React.FC<LocalizedPage> = async (properties) => {
+  const { locale } = properties;
+  const payload = await getPayload({ config });
+  const { content } = await payload.findGlobal({
+    slug: 'data-privacy-statement',
+    locale,
+  });
+
   return (
-    <>
-      <span>Render Privacy Page in {properties.locale}</span>
-    </>
+    <article className="mx-auto my-8 max-w-5xl px-8">
+      <HeadlineH1>{content.pageTitle}</HeadlineH1>
+      <LexicalPageContent pageContent={content.mainContent as SerializedEditorState} />
+    </article>
   );
 };
