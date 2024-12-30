139fbcfce805b3ac0029db401a28f95f9ad05975
Cyrill Püntener <wp99cp@gmx.net>
2024-12-30 10:03:14 +0100

    fix rendering of blog article

--- src/converters/blog-article/index.tsx
+++ src/converters/blog-article/index.tsx
@@ -5,15 +5,15 @@ import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical
 import React from 'react';
 import Image from 'next/image';
 
-export const BlogArticle: React.FC<{ article: Blog }> = ({ page }) => {
-  if (typeof page.content.bannerImage === 'string') {
+export const BlogArticle: React.FC<{ article: Blog }> = ({ article }) => {
+  if (typeof article.content.bannerImage === 'string') {
     throw new TypeError(
       'Expected bannerImage to be an object, you may got the ID instead of the object',
     );
   }
 
-  const source = page.content.bannerImage.url ?? '/images/placeholder.png';
-  const altText = page.content.bannerImage.alt;
+  const source = article.content.bannerImage.url ?? '/images/placeholder.png';
+  const altText = article.content.bannerImage.alt;
 
   return (
     <>
@@ -22,8 +22,8 @@ export const BlogArticle: React.FC<{ article: Blog }> = ({ page }) => {
           <Image objectFit="contain" layout="fill" src={source} alt={altText} fill={true} />
         </div>
 
-        <HeadlineH1>{page.content.blogH1}</HeadlineH1>
-        <LexicalPageContent pageContent={page.content.pageContent as SerializedEditorState} />
+        <HeadlineH1>{article.content.blogH1}</HeadlineH1>
+        <LexicalPageContent pageContent={article.content.pageContent as SerializedEditorState} />
       </article>
     </>
   );
