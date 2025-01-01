43c22458d59deea87fec0e7216ade797047cb868
Cyrill Püntener <wp99cp@gmx.net>
2025-01-01 13:51:29 +0100

    inline header logo svg into HTML content for faster page load

--- src/components/header/header-component.tsx
+++ src/components/header/header-component.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import React from 'react';
 import { NavComponent } from '@/components/menu/nav-component';
+import { ConveniatLogo } from '@/components/svg-logos/conveniat-logo';
 
 export const HeaderComponent: React.FC = () => {
   return (
@@ -10,13 +10,7 @@ export const HeaderComponent: React.FC = () => {
         <div className="relative mx-auto h-[60px] w-full max-w-2xl text-conveniat-green">
           <div className="flex items-center justify-between px-6">
             <Link href="/">
-              <Image
-                src="/favicon.svg"
-                alt="Conveniat 2027 Logo"
-                width={80}
-                height={80}
-                className="absolute left-[24px] top-[12px] z-[100]"
-              />
+              <ConveniatLogo className="absolute left-[24px] top-[12px] z-[100]" />
             </Link>
             <span className="absolute left-0 top-[16px] hidden w-full items-center justify-center text-[24px] font-extrabold leading-normal opacity-0 sm:flex sm:opacity-100">
               Conveniat 2027
