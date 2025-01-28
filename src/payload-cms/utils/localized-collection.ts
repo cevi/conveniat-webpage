5a88fe48a536e5b37aec392ecb07a13f9e9cba46
Cyrill Püntener <wp99cp@gmx.net>
2025-01-28 18:06:29 +0100

    remove broken access control

--- src/payload-cms/utils/localized-collection.ts
+++ src/payload-cms/utils/localized-collection.ts
@@ -1,4 +1,4 @@
-import { CollectionConfig, FieldAccess } from 'payload';
+import { CollectionConfig } from 'payload';
 import { localizedStatusSchema } from '@/payload-cms/utils/localized-status-schema';
 import { getPublishingStatus } from '@/payload-cms/hooks/publishing-status';
 
@@ -82,21 +82,13 @@ export const asLocalizedCollection = (config: CollectionConfig): CollectionConfi
         admin: {
           disabled: true,
         },
-        access: {
-          update: (({ doc }): boolean =>
-            (doc as { _disable_unpublishing: boolean } | undefined)?.['_disable_unpublishing'] ===
-            false) as FieldAccess,
-        },
       },
 
       {
         name: '_disable_unpublishing',
         type: 'checkbox',
-        access: {
-          update: () => false,
-        },
         admin: {
-          hidden: true,
+          disabled: true,
         },
         localized: false,
         defaultValue: false,
