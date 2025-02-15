00e88a6a30d9eb0f05374beb411ca7c043c06503
Cyrill Püntener <wp99cp@gmx.net>
2025-02-15 15:34:57 +0100

    style login page

--- src/payload-cms/components/login-page/login-button.tsx
+++ src/payload-cms/components/login-page/login-button.tsx
@@ -1,6 +1,9 @@
 'use client';
 import React from 'react';
 import { signIn } from 'next-auth/react';
+import { useLocale } from '@payloadcms/ui';
+import { Config } from '@/payload-types';
+import { CeviLogo } from '@/components/svg-logos/cevi-logo';
 
 const handleLoginClick = (): void => {
   signIn('cevi-db').catch((error: unknown) => {
@@ -9,7 +12,34 @@ const handleLoginClick = (): void => {
 };
 
 const LoginButton: React.FC = () => {
-  return <button onClick={handleLoginClick}>Login with CeviDB</button>;
+  const { code } = useLocale() as { code: Config['locale'] };
+
+  const localizedLoginText: {
+    [key in Config['locale']]: string;
+  } = {
+    en: 'Login with CeviDB',
+    de: 'Mit CeviDB anmelden',
+    fr: 'Se connecter avec CeviDB',
+  };
+
+  return (
+    <>
+      {/* background logo */}
+      <div className="fixed left-0 top-0 z-[-999] h-screen w-full bg-[#f8fafc] p-[56px]">
+        <CeviLogo className="admin-panel-blur mx-auto h-full max-h-[60vh] w-full max-w-[384px] opacity-10" />
+      </div>
+
+      {/* login button */}
+      <div className="flex justify-center">
+        <button
+          className="rounded border-2 border-solid border-green-600 bg-green-100 px-4 py-2 font-bold text-conveniat-green outline-none"
+          onClick={handleLoginClick}
+        >
+          {localizedLoginText[code]}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default LoginButton;
