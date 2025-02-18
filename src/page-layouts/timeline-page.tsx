cac19e912bc034867658dc31d8a717779daf8288
maede97 <maede97@hotmail.com>
2025-02-18 17:35:45 +0100

    work on timeline

--- src/page-layouts/timeline-page.tsx
+++ src/page-layouts/timeline-page.tsx
@@ -4,11 +4,8 @@ import config from '@payload-config';
 import { HeadlineH1 } from '@/components/typography/headline-h1';
 import { PageSectionsConverter, ContentBlock } from 'src/converters/page-sections';
 import { LocalizedCollectionPage, StaticTranslationString } from '@/types';
-
-const isActive = (date: string) => {
-  // if date is in the past, return 'is-active', else return ''
-  return new Date(date) < new Date() ? 'is-active' : '';
-};
+import { SubheadingH2 } from '@/components/typography/subheading-h2';
+import { ParagraphText } from '@/components/typography/paragraph-text';
 
 const pageTitle: StaticTranslationString = {
   en: 'Timeline',
@@ -33,11 +30,13 @@ export const TimeLinePage: React.FC<LocalizedCollectionPage> = async ({ locale, 
   });
 
   const convertDate = (date: string) => {
-    // i want dd.mm.yyyy
+    // i want dd month yyyy, hh:mm Uhr
     const options: Intl.DateTimeFormatOptions = {
       year: 'numeric',
-      month: '2-digit',
+      month: 'long',
       day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
     };
     return new Date(date).toLocaleDateString(locale, options);
   };
@@ -45,40 +44,21 @@ export const TimeLinePage: React.FC<LocalizedCollectionPage> = async ({ locale, 
   return (
     <article className="mx-auto my-8 max-w-2xl px-8">
       <HeadlineH1>{pageTitle[locale]}</HeadlineH1>
+      {timeLineItems.docs.map((item) => {
+        return (
+        <div key={item.date}>
+          <ParagraphText className="my-0">{convertDate(item.date)}</ParagraphText>
+          <div className="border-l-green-100 border-l-4 p-4 m-2">
+            <SubheadingH2 className='m-0'>{item.title}</SubheadingH2>
 
-      <div className="before:to-transparent relative space-y-8 before:absolute before:inset-0 before:ml-5 before:h-full before:w-0.5 before:-translate-x-px before:bg-gradient-to-b before:from-conveniat-green before:via-conveniat-green md:before:mx-auto md:before:translate-x-0">
-        {timeLineItems.docs.map((item) => {
-          const isItemActive = isActive(item.date);
-          return (
-            <div
-              key={item.date}
-              className={`group relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse ${isActive(item.date)}`}
-            >
-              <div
-                className={`${
-                  isItemActive ? 'bg-cevi-blue text-white' : 'bg-cevi-red'
-                } flex h-10 w-10 shrink-0 items-center justify-center rounded-full shadow md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2`}
-              ></div>
-
-              <div className="border-slate-200 w-[calc(100%-4rem)] rounded border bg-white p-4 shadow md:w-[calc(50%-2.5rem)]">
-                <div className="mb-1 flex items-center justify-between space-x-2">
-                  <div className="text-slate-900 font-bold">{item.title}</div>
-                  <time className="font-caveat text-indigo-500 font-medium">
-                    {convertDate(item.date)}
-                  </time>
-                </div>
-                <div>
-                  <PageSectionsConverter
-                    blocks={item.mainContent as ContentBlock[]}
-                    locale={locale}
-                    searchParams={searchParams}
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            <PageSectionsConverter
+              blocks={item.mainContent as ContentBlock[]}
+              locale={locale}
+              searchParams={searchParams}
+            />
+          </div>
+        </div>
+      )})}
     </article>
   );
 };
