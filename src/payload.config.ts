adac0b1d19a650b4cfdff8e5c99ec491772cc86f
Cyrill Püntener <wp99cp@gmx.net>
2025-02-23 14:32:25 +0100

    add smartphoneBreakpoints

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -28,6 +28,62 @@ const DATABASE_URI = process.env['DATABASE_URI'] ?? '';
 const APP_HOST_URL = process.env['APP_HOST_URL'] ?? '';
 //const APP_HOST_URL = process.env['APP_HOST_URL'] ?? ''; // not needed as live-preview is currently disabled
 
+const smartphoneBreakpoints: {
+  height: number | string;
+  label: string;
+  name: string;
+  width: number | string;
+}[] = [
+  {
+    name: 'iphone-se',
+    label: 'iPhone SE (375x667)',
+    width: 375,
+    height: 667,
+  },
+  {
+    name: 'iphone-xr',
+    label: 'iPhone XR (414x896)',
+    width: 414,
+    height: 896,
+  },
+  {
+    name: 'iphone-14',
+    label: 'iPhone 14 (390x844)',
+    width: 390,
+    height: 844,
+  },
+  {
+    name: 'iphone-14-pro',
+    label: 'iPhone 14 Pro (393x852)',
+    width: 393,
+    height: 852,
+  },
+  {
+    name: 'iphone-14-pro-max',
+    label: 'iPhone 14 Pro Max (430x932)',
+    width: 430,
+    height: 932,
+  },
+  {
+    name: 'google-pixel-6',
+    label: 'Google Pixel 6 (393x851)',
+    width: 393,
+    height: 851,
+  },
+  {
+    name: 'google-pixel-7',
+    label: 'Google Pixel 7 (412x870)',
+    width: 412,
+    height: 870,
+  },
+  {
+    name: 'google-pixel-7-pro',
+    label: 'Google Pixel 7 Pro (412x892)',
+    width: 412,
+    height: 892,
+  },
+];
+
 /**
  * Generates the preview URL for the live preview feature.
  *
@@ -103,6 +159,7 @@ export const payloadConfig: RoutableConfig = {
 
     livePreview: {
       url: generatePreviewUrl,
+      breakpoints: smartphoneBreakpoints,
       collections: ['blog', 'generic-page', 'timeline'],
     },
   },
