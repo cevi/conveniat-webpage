a76109237be9c38a7ab255d923e8002b82abd939
Cyrill Püntener <wp99cp@gmx.net>
2025-02-23 14:53:18 +0100

    fix formating (this line is somehow broken...)

--- src/app/(frontend)/[locale]/[[...slugs]]/page.tsx
+++ src/app/(frontend)/[locale]/[[...slugs]]/page.tsx
@@ -82,7 +82,7 @@ const canAccessPreviewOfCurrentPage = async (
 const PreviewWarning: React.FC = () => {
   return (
     <div className="fixed bottom-0 right-0 z-50 p-4">
-      <div className="bg-orange-500 rounded-lg px-4 py-2 font-bold text-white shadow-lg">
+      <div className="rounded-lg bg-orange-500 px-4 py-2 font-bold text-white shadow-lg">
         THIS IS A PREVIEW
       </div>
     </div>
