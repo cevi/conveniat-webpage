bc0578bf634b35e661576d83946c472961de0ec0
Cyrill Püntener <wp99cp@gmx.net>
2025-02-25 10:20:46 +0100

    fix header font type

--- src/components/header/header-component.tsx
+++ src/components/header/header-component.tsx
@@ -18,10 +18,10 @@ export const HeaderComponent: React.FC = async () => {
             <Link href="/">
               <ConveniatLogo className="absolute left-[24px] top-[12px] z-[100]" />
             </Link>
-            <span className="absolute left-0 top-[16px] hidden w-full items-center justify-center text-[24px] font-extrabold leading-normal opacity-0 sm:flex sm:opacity-100">
+            <span className="absolute left-0 top-[16px] hidden w-full items-center justify-center font-['Montserrat'] text-[24px] font-extrabold leading-normal opacity-0 sm:flex sm:opacity-100">
               conveniat27
             </span>
-            <span className="absolute left-0 top-[16px] flex w-full items-center justify-center text-[24px] font-extrabold leading-normal sm:hidden">
+            <span className="absolute left-0 top-[16px] flex w-full items-center justify-center font-['Montserrat'] text-[24px] font-extrabold leading-normal sm:hidden">
               conveniat27
             </span>
             <NavComponent />
