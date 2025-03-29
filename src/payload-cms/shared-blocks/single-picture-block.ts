621e24f82c4b71e54fd6be94ecc04967e63d3e64
Cyrill Püntener <wp99cp@gmx.net>
2025-03-29 15:22:16 +0100

    add single picture (#176)

---
+++ src/payload-cms/shared-blocks/single-picture-block.ts
@@ -0,0 +1,19 @@
+import { Block } from 'payload';
+
+export const singlePictureBlock: Block = {
+  slug: 'singlePicture',
+
+  imageURL: '/admin-block-images/single-picture-block.png',
+  imageAltText: 'Single picture block',
+
+  fields: [
+    {
+      name: 'image',
+      label: 'Images',
+      type: 'relationship',
+      relationTo: 'images',
+      hasMany: false,
+      required: true,
+    },
+  ],
+};
