6704351ed8e280f194085593d72f6853eef228cd
Cyrill Püntener <wp99cp@gmx.net>
2025-04-15 14:44:43 +0200

    merge back service worker files for offline support

--- src/service-worker/offline-support.ts
+++ src/service-worker/offline-support.ts
@@ -1,5 +1,25 @@
-import { RouteHandler } from 'serwist';
-import { serwistFactory } from '@/service-worker/serwist';
+import { PrecacheEntry, RouteHandler, Serwist } from 'serwist';
+import { defaultCache } from '@serwist/next/worker';
+
+/**
+ * Serwist factory function.
+ *
+ * @param self - The service worker global scope.
+ */
+const serwistFactory = (self: ServiceWorkerGlobalScope): Serwist => {
+  return new Serwist({
+    precacheEntries: self.__SW_MANIFEST as (string | PrecacheEntry)[],
+    precacheOptions: {
+      cleanupOutdatedCaches: true,
+      concurrency: 10,
+      ignoreURLParametersMatching: [],
+    },
+    skipWaiting: true,
+    clientsClaim: true,
+    navigationPreload: true,
+    runtimeCaching: defaultCache,
+  });
+};
 
 /**
  * If a Route throws an error while handling a request,
