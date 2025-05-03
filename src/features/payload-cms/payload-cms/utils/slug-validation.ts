a58b3497151a99464608d3e6c87ddf3078076b33
maede97 <maede97@hotmail.com>
2025-05-03 10:30:13 +0200

    implement #163

--- src/features/payload-cms/payload-cms/utils/slug-validation.ts
+++ src/features/payload-cms/payload-cms/utils/slug-validation.ts
@@ -15,6 +15,8 @@ export const slugValidation: TextFieldSingleValidation = async (value, arguments
   if (!/^[a-z0-9-]+$/.test(value))
     return 'Slug can only contain lowercase letters, numbers, and hyphens';
 
+  if (/^(api|admin|app)$/.test(value)) return 'Slug cannot be api, admin or app';
+
   if (value.length < slugMinLength) return `Slug must be at least ${slugMinLength} characters long`;
   if (value.length > slugMaxLength) return 'Slug cannot be longer than ${slugMaxLength} characters';
 
