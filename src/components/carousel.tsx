5c697817264dcb458c7e178af606d28ca8477e89
Cyrill Püntener <wp99cp@gmx.net>
2025-05-05 20:06:11 +0200

    add wide screen layout for gallery

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -226,7 +226,7 @@ const CarouselPrevious = React.forwardRef<HTMLButtonElement, React.ComponentProp
         className={cn(
           'absolute h-8 w-8 rounded-full',
           orientation === 'horizontal'
-            ? '-left-12 top-[140px] -translate-y-1/2'
+            ? '-left-12 top-[140px] xl:top-[200px] -translate-y-1/2'
             : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
           className,
         )}
@@ -254,7 +254,7 @@ const CarouselNext = React.forwardRef<HTMLButtonElement, React.ComponentProps<ty
         className={cn(
           'absolute h-8 w-8 rounded-full',
           orientation === 'horizontal'
-            ? '-right-12 top-[140px] -translate-y-1/2'
+            ? '-right-12 top-[140px] xl:top-[200px] -translate-y-1/2'
             : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
           className,
         )}
