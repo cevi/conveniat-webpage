45bfbc073c17124214cacb2d5cd8af7192265f81
Cyrill Püntener <wp99cp@gmx.net>
2025-05-08 12:59:58 +0200

    fix layout issues

--- src/features/payload-cms/components/content-blocks/list-blog-articles.tsx
+++ src/features/payload-cms/components/content-blocks/list-blog-articles.tsx
@@ -67,7 +67,7 @@ export const ListBlogPosts: React.FC<LocalizedPageType> = async ({ locale }) => 
   const blogs = blogsPaged.docs as Blog[];
 
   return (
-    <div className="mx-auto flex flex-col my-8 xl:my-16 2xl:my-20xl:mx-[-10rem] 2xl:mx-[-20rem]">
+    <div className="mx-auto flex flex-col my-8 xl:my-16 2xl:my-20xl:mx-[-10rem] 2xl:mx-[-20rem] 2xl:px-8">
       <h2
         id="voluptatibus-odit-quam-nam-placeat-sed"
         className="mb-2 mt-8 max-w-4xl text-balance font-heading text-lg font-extrabold text-conveniat-green"
