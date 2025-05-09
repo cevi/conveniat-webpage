3978e7becb193f2abc0b957a8d508004262f91ac
Cyrill Püntener <wp99cp@gmx.net>
2025-05-09 16:32:50 +0200

    remove waiting

--- src/app/(frontend)/[locale]/(non-payload-pages)/app/chat/page.tsx
+++ src/app/(frontend)/[locale]/(non-payload-pages)/app/chat/page.tsx
@@ -1,7 +1,10 @@
 import { ChatsOverviewComponent } from '@/features/chat/components/chat-overview/chats-overview-component';
 import type React from 'react';
 
-const ChatPage: React.FC = () => {
+const ChatPage: React.FC = async () => {
+  // wait 20 seconds to simulate loading
+  await new Promise((resolve) => setTimeout(resolve, 20_000));
+
   return <ChatsOverviewComponent />;
 };
 
