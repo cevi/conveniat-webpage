99a8282b95dae5ddef6d7a3e604eaa89fd04eeac
Cyrill Püntener <wp99cp@gmx.net>
2025-05-09 11:43:31 +0200

    fix middleware (app feature toggle was broken)

---
+++ src/middleware/middleware-chain.ts
@@ -0,0 +1,28 @@
+import type { NextMiddlewareResult } from 'next/dist/server/web/types';
+import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export type ChainedMiddleware = (
+  request: NextRequest,
+  event: NextFetchEvent,
+  response: NextResponse | undefined,
+) => NextMiddlewareResult | Promise<NextMiddlewareResult>;
+
+type MiddlewareFactory = (middleware: ChainedMiddleware) => ChainedMiddleware;
+
+/**
+ * based on https://medium.com/@0xTanzim/implementing-multiple-middleware-in-next-js-combining-nextauth-and-internationalization-28d5435d3187
+ * @param functions
+ * @param index
+ */
+export function middlewareChain(functions: MiddlewareFactory[], index = 0): ChainedMiddleware {
+  const current = functions[index];
+
+  if (current) {
+    const next = middlewareChain(functions, index + 1);
+    return current(next);
+  }
+
+  return (_request: NextRequest, _event: NextFetchEvent, response: NextResponse) => {
+    return response;
+  };
+}
