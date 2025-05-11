b660c85fa31960cf139c54e6728f4258e4ad51b5
Cyrill Püntener <wp99cp@gmx.net>
2025-05-11 16:36:52 +0200

    remove Button wrapper

--- src/features/chat/components/chat-header.tsx
+++ src/features/chat/components/chat-header.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/buttons/button';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 import type React from 'react';
@@ -11,9 +10,7 @@ export const ChatHeader: React.FC<ChatHeaderProperties> = ({ name }) => {
   return (
     <div className="flex items-center gap-2 border-b-2 border-gray-200 px-4 h-[62px] dark:border-gray-700">
       <Link href="/app/chat">
-        <Button variant="ghost" size="icon" className="mr-2">
-          <ArrowLeft className="h-5 w-5" />
-        </Button>
+        <ArrowLeft className="h-5 w-5 mr-2" />
       </Link>
       <h1 className="text-xl font-semibold">{name}</h1>
     </div>
