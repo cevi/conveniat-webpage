51aedee11424ca82fa8720b074d52237ae47563b
Cyrill Püntener <wp99cp@gmx.net>
2025-05-11 17:06:36 +0200

    increase link target for chat details

--- src/features/chat/components/chat-overview/chat-preview.tsx
+++ src/features/chat/components/chat-overview/chat-preview.tsx
@@ -9,19 +9,21 @@ export const ChatPreview: React.FC<{
   const chatDetailLink = `chat/${chat.id}`;
 
   return (
-    <li className="cursor-pointer bg-none p-4 hover:bg-gray-100">
-      <div className="flex items-center space-x-4">
-        <div className="flex-shrink-0">
-          <UsersRound size={32} />
+    <Link href={chatDetailLink}>
+      <li className="cursor-pointer bg-none p-4 hover:bg-gray-100">
+        <div className="flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <UsersRound size={32} />
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="truncate text-sm font-medium text-gray-900">{chat.name}</p>
+            <p className="truncate text-sm text-gray-500">{chat.lastMessage?.content}</p>
+          </div>
+          <div className="flex-shrink-0">
+            <MessageSquare size={20} className="text-gray-400" />
+          </div>
         </div>
-        <Link className="min-w-0 flex-1" href={chatDetailLink}>
-          <p className="truncate text-sm font-medium text-gray-900">{chat.name}</p>
-          <p className="truncate text-sm text-gray-500">{chat.lastMessage?.content}</p>
-        </Link>
-        <div className="flex-shrink-0">
-          <MessageSquare size={20} className="text-gray-400" />
-        </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 };
