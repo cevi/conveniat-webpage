9c751424bbb3791c16c185d41514f87d13adf571
maede97 <maede97@hotmail.com>
2025-05-31 11:56:34 +0200

    show only forms when they are published in the current locale

--- src/features/payload-cms/payload-cms/shared-blocks/form-block.ts
+++ src/features/payload-cms/payload-cms/shared-blocks/form-block.ts
@@ -1,4 +1,32 @@
-import type { Block } from 'payload';
+import type { Locale } from '@/types/types';
+import type { Block, FilterOptionsProps, Where } from 'payload';
+
+const findQuery: ({ req, relationTo }: FilterOptionsProps<unknown>) => Promise<Where> = async ({
+  req,
+  relationTo,
+}) => {
+  const { payload, locale } = req;
+
+  const allItems = await payload.find({
+    collection: relationTo,
+    draft: false,
+    locale: locale as Locale,
+    limit: 10,
+    where: {
+      _localized_status: {
+        equals: {
+          published: true,
+        },
+      },
+    },
+  });
+  const allIds = allItems.docs.map((item) => item.id);
+  return {
+    id: {
+      in: allIds,
+    },
+  };
+};
 
 export const formBlock: Block = {
   slug: 'formBlock',
@@ -13,6 +41,8 @@ export const formBlock: Block = {
       type: 'relationship',
       relationTo: 'forms',
       required: true,
+      hasMany: false,
+      filterOptions: findQuery,
     },
   ],
 };
