388946abd6e9688d4a4808f34b1376f919fa1e87
Cyrill Püntener <wp99cp@gmx.net>
2025-05-31 11:26:10 +0200

    add seeding for main menu

--- src/features/payload-cms/payload-cms/globals/header-global.ts
+++ src/features/payload-cms/payload-cms/globals/header-global.ts
@@ -1,6 +1,27 @@
 import { AdminPanelDashboardGroups } from '@/features/payload-cms/payload-cms/admin-panel-dashboard-groups';
 import type { Field, GlobalConfig, TextFieldSingleValidation } from 'payload';
 
+const validateLink: TextFieldSingleValidation = (link) => {
+  // Check if the link is provided
+  if (link === null || link === undefined || link.trim() === '') {
+    return 'Link is required';
+  }
+
+  // Validate the URL format, starting with / or https://
+  const urlPattern = /^(https?:\/\/|\/)[^\s/$.?#].[^\s]*$/;
+  if (!urlPattern.test(link)) {
+    return 'Link must be a valid URL starting with https:// or /';
+  }
+
+  return true; // Valid link
+};
+
+interface SubMenuItem {
+  label: string;
+  link: string;
+  isExternal: boolean;
+}
+
 /**
  * Validates the link field based on the presence and content of sub-menu items.
  *
@@ -11,22 +32,18 @@ import type { Field, GlobalConfig, TextFieldSingleValidation } from 'payload';
  *
  * - If a link is provided, it **must be a valid URL** starting with "https://".
  *
- * @param value
- * @param siblingData
+ * @param link
+ * @param options
  */
-const validateLinkWithNested: TextFieldSingleValidation = (
-  value,
-  {
-    siblingData,
-  }: {
-    siblingData: {
-      subMenu?: { label: string; link: string; isExternal: boolean }[];
-    };
-  },
-) => {
-  const hasSubMenuItems: boolean =
-    siblingData.subMenu !== undefined && siblingData.subMenu.length > 0;
-  const isLinkProvided: boolean = value !== undefined && value !== null && value !== '';
+const validateLinkWithNested: TextFieldSingleValidation = (link, options) => {
+  const subMenu = (
+    options.siblingData as {
+      subMenu?: SubMenuItem[] | undefined;
+    }
+  ).subMenu;
+
+  const hasSubMenuItems: boolean = subMenu !== undefined && subMenu.length > 0;
+  const isLinkProvided: boolean = link !== undefined && link !== null && link !== '';
 
   if (hasSubMenuItems) {
     if (isLinkProvided) return 'Link must be empty if sub-menu items are present';
@@ -35,11 +52,7 @@ const validateLinkWithNested: TextFieldSingleValidation = (
     // No sub-menu items are present.
     if (!isLinkProvided) return 'Link is required if no sub-menu items are present';
 
-    // Link is provided and no sub-menu items, so validate the URL format.
-    if (!/^https?:\/\/[^\s/$.?#].[^\s]*$/.test(value ?? '')) {
-      return 'Link must be a valid URL starting https://';
-    }
-    return true; // Valid: no sub-menu, link provided, and URL format is correct.
+    return validateLink(link, options); // Validate the link format.
   }
 };
 
@@ -92,6 +105,7 @@ const MainMenu: Field = {
           name: 'link',
           label: 'Link',
           type: 'text',
+          validate: validateLink,
           required: true,
         },
         {
---
+++ src/features/payload-cms/payload-cms/initialization/seeding/generate-main-menu.ts
@@ -0,0 +1,22 @@
+import { fakerDE as faker } from '@faker-js/faker';
+
+export const generateMainMenu = (count: number = 3) => {
+  const menuItems = [];
+  for (let index = 0; index < count; index++) {
+    const hasSubMenu = faker.datatype.boolean();
+    const menuItem = {
+      label: faker.lorem.words(faker.number.int({ min: 1, max: 3 })),
+      link: hasSubMenu ? '' : `/${faker.lorem.slug()}`,
+      isExternal: false,
+      subMenu: hasSubMenu
+        ? Array.from({ length: faker.number.int({ min: 1, max: 2 }) }).map(() => ({
+            label: faker.lorem.words(faker.number.int({ min: 1, max: 3 })),
+            link: `/${faker.lorem.slug()}`,
+            isExternal: false,
+          }))
+        : [],
+    };
+    menuItems.push(menuItem);
+  }
+  return menuItems;
+};
