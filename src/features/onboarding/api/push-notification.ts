6830776f23afd63d3d085afaa8b1d830d4e87a27
maede97 <maede97@hotmail.com>
2025-05-31 13:59:00 +0200

    work on #249

--- src/features/onboarding/api/push-notification.ts
+++ src/features/onboarding/api/push-notification.ts
@@ -46,40 +46,43 @@ export async function subscribeUser(
   // eslint-disable-next-line unicorn/no-null
   const payloadUser = (await getPayloadUserFromNextAuthUser(payload, hitobito_user)) ?? null;
 
-  const isProdDeployment =
-    environmentVariables.NEXT_PUBLIC_APP_HOST_URL?.includes('conveniat27.cevi.tools'); // TODO: change when tested to prod instance
+  const isProductionDeployment =
+    environmentVariables.NEXT_PUBLIC_APP_HOST_URL.includes('conveniat27.cevi.tools');
 
-  if (!payloadUser) {
-    // create a new one
-    await payload.create({
-      collection: 'push-notification-subscriptions',
-      data: { ...sub, user: null },
-    });
-  } else {
+  if (payloadUser) {
     // check if the user already has a subscription
     const existingSubscription = await payload.find({
       collection: 'push-notification-subscriptions',
       where: {
         user: {
-          equals: payloadUser?.id,
+          equals: payloadUser.id,
         },
       },
     });
 
-    if (isProdDeployment && existingSubscription.totalDocs > 0) {
+    if (isProductionDeployment && existingSubscription.totalDocs > 0) {
       // if the user already has a subscription, we update it
+      const existingSubscriptionId = existingSubscription.docs[0]?.id as string;
       await payload.update({
         collection: 'push-notification-subscriptions',
-        id: existingSubscription.docs[0]?.id ?? '',
-        data: { ...sub, user: payloadUser },
+        id: existingSubscriptionId,
+        data: {
+          ...sub,
+          user: payloadUser,
+        },
       });
-      return { success: true };
     } else {
       await payload.create({
         collection: 'push-notification-subscriptions',
         data: { ...sub, user: payloadUser },
       });
     }
+  } else {
+    // create a new one
+    await payload.create({
+      collection: 'push-notification-subscriptions',
+      data: { ...sub },
+    });
   }
 
   // send a test notification to the user
