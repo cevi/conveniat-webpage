b032cc126a4e15058a9cf694d07e0a7af892fc4f
Cyrill Püntener <wp99cp@gmx.net>
2025-06-01 11:05:57 +0200

    improve layout for wide screens

--- src/app/(frontend)/[locale]/(payload-pages)/layout.tsx
+++ src/app/(frontend)/[locale]/(payload-pages)/layout.tsx
@@ -9,7 +9,9 @@ interface LayoutProperties {
 const Layout: React.FC<LayoutProperties> = async ({ children }) => {
   return (
     <>
-      <div className="min-h-full min-w-full overflow-x-hidden xl:px-12">{children}</div>
+      <div className="min-h-full min-w-full overflow-x-hidden pt-8 pb-24 min-[1280px]:px-12 min-[1800px]:px-18 min-[2400px]:px-96">
+        {children}
+      </div>
       <div></div>
       <FooterComponent />
     </>
