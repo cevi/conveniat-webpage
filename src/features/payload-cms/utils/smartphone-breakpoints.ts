af0cd5cbe4da9941fe1803b4ee8eab0772c6c911
maede97 <maede97@hotmail.com>
2025-06-01 15:46:45 +0200

    move smartphone breakpoints to its own file

---
+++ src/features/payload-cms/utils/smartphone-breakpoints.ts
@@ -0,0 +1,55 @@
+export const smartphoneBreakpoints: {
+  height: number | string;
+  label: string;
+  name: string;
+  width: number | string;
+}[] = [
+  {
+    name: 'iphone-se',
+    label: 'iPhone SE (375x667)',
+    width: 375,
+    height: 667,
+  },
+  {
+    name: 'iphone-xr',
+    label: 'iPhone XR (414x896)',
+    width: 414,
+    height: 896,
+  },
+  {
+    name: 'iphone-14',
+    label: 'iPhone 14 (390x844)',
+    width: 390,
+    height: 844,
+  },
+  {
+    name: 'iphone-14-pro',
+    label: 'iPhone 14 Pro (393x852)',
+    width: 393,
+    height: 852,
+  },
+  {
+    name: 'iphone-14-pro-max',
+    label: 'iPhone 14 Pro Max (430x932)',
+    width: 430,
+    height: 932,
+  },
+  {
+    name: 'google-pixel-6',
+    label: 'Google Pixel 6 (393x851)',
+    width: 393,
+    height: 851,
+  },
+  {
+    name: 'google-pixel-7',
+    label: 'Google Pixel 7 (412x870)',
+    width: 412,
+    height: 870,
+  },
+  {
+    name: 'google-pixel-7-pro',
+    label: 'Google Pixel 7 Pro (412x892)',
+    width: 412,
+    height: 892,
+  },
+];
