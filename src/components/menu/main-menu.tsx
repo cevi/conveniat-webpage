b062e6e113e4b0d14f537c7463e46d2b3c920463
maede97 <maede97@hotmail.com>
2025-06-01 12:29:20 +0200

    try fixing #279

--- src/components/menu/main-menu.tsx
+++ src/components/menu/main-menu.tsx
@@ -25,70 +25,68 @@ export const MainMenu: React.FC = async () => {
 
   return (
     <div className="mx-auto mt-8 flex h-[calc(100%-100px)] max-w-md flex-col justify-between divide-gray-100">
-      <div>
-        <span className="text-conveniat-green hidden w-full font-['Montserrat'] text-[24px] leading-normal font-extrabold xl:block">
-          <Link key="home" href="/">
-            conveniat27
-          </Link>
-        </span>
-        <div className="space-y-2 py-6">
-          {mainMenu.map((item) => {
-            if (item.subMenu && item.subMenu.length > 0) {
+      <div className="mr-[-10px] w-full flex-1 overflow-x-hidden overflow-y-auto pr-[10px]">
+        <div>
+          <span className="text-conveniat-green hidden w-full font-['Montserrat'] text-[24px] leading-normal font-extrabold xl:block">
+            <Link key="home" href="/">
+              conveniat27
+            </Link>
+          </span>
+          <div className="space-y-2 py-6">
+            {mainMenu.map((item) => {
+              if (item.subMenu && item.subMenu.length > 0) {
+                return (
+                  <Disclosure key={item.id} as="div" className="-mx-3">
+                    <DisclosureButton className="group flex w-full cursor-pointer items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-700 hover:bg-gray-50">
+                      {item.label}
+                      <ChevronDown
+                        aria-hidden="true"
+                        className="size-5 flex-none group-data-open:rotate-180"
+                      />
+                    </DisclosureButton>
+                    <DisclosurePanel className="mt-2 space-y-2">
+                      {item.subMenu.map((subItem) => (
+                        <Link
+                          key={subItem.id}
+                          href={subItem.link}
+                          className="closeNavOnClick block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-500 hover:bg-gray-50"
+                        >
+                          {subItem.label}
+                        </Link>
+                      ))}
+                    </DisclosurePanel>
+                  </Disclosure>
+                );
+              }
+
+              if (item.link === undefined || item.link === null) return <></>;
+
               return (
-                <Disclosure key={item.id} as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full cursor-pointer items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-700 hover:bg-gray-50">
+                <Link key={item.id} href={item.link}>
+                  <span className="closeNavOnClick -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-700 hover:bg-gray-50">
                     {item.label}
-                    <ChevronDown
-                      aria-hidden="true"
-                      className="size-5 flex-none group-data-open:rotate-180"
-                    />
-                  </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
-                    {item.subMenu.map((subItem) => (
-                      <Link
-                        key={subItem.id}
-                        href={subItem.link}
-                        className="closeNavOnClick block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-500 hover:bg-gray-50"
-                      >
-                        {subItem.label}
-                      </Link>
-                    ))}
-                  </DisclosurePanel>
-                </Disclosure>
+                  </span>
+                </Link>
               );
-            }
-
-            if (item.link === undefined || item.link === null) return <></>;
-
-            // If the item has no sub-menu, render a simple link
-            return (
-              <Link key={item.id} href={item.link}>
-                <span className="closeNavOnClick -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-700 hover:bg-gray-50">
-                  {item.label}
-                </span>
-              </Link>
-            );
-          })}
+            })}
+          </div>
         </div>
-      </div>
 
-      <hr className="divide-y-2 divide-gray-100 xl:hidden" />
+        <hr className="divide-y-2 divide-gray-100 xl:hidden" />
 
-      <div className="divide-y-2 divide-gray-100">
-        <MainMenuLanguageSwitcher locale={locale} />
+        <div className="divide-y-2 divide-gray-100 py-4">
+          <MainMenuLanguageSwitcher locale={locale} />
+          <SearchComponent locale={locale} actionURL={actionURL} />
 
-        <SearchComponent locale={locale} actionURL={actionURL} />
-
-        {isInAppDesign && build && (
-          <>
+          {isInAppDesign && build && (
             <div className="flex flex-col py-6 text-center">
               <FooterBuildInfoText>Version {build.version} </FooterBuildInfoText>
               <FooterBuildInfoText>
                 Build {build.git.hash} vom {build.timestamp}
               </FooterBuildInfoText>
             </div>
-          </>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
