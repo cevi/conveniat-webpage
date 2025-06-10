1b8d80a5b753703a231175a6725283edad94ffbd
Cyrill Püntener <wp99cp@gmx.net>
2025-06-10 09:57:31 +0200

    fix layout issues

--- src/app/(frontend)/[locale]/(payload-pages)/layout.tsx
+++ src/app/(frontend)/[locale]/(payload-pages)/layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProperties {
 const Layout: React.FC<LayoutProperties> = async ({ children }) => {
   return (
     <>
-      <div className="min-h-full min-w-full overflow-x-hidden pt-8 pb-24 min-[1280px]:px-12 min-[1800px]:px-18 min-[2400px]:px-96">
+      <div className="min-h-[calc(100dvh-280px)] min-w-full overflow-x-hidden overflow-y-clip pt-8 pb-24 min-[1280px]:px-12 min-[1800px]:px-18 min-[2400px]:px-96">
         {children}
       </div>
       <div></div>
