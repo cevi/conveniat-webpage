e65c3755db01b69c2345532247bbbdff02068143
maede97 <maede97@hotmail.com>
2025-07-26 11:00:17 +0200

    reduce arrow complexity of number

--- src/features/payload-cms/components/form/number.tsx
+++ src/features/payload-cms/components/form/number.tsx
@@ -9,37 +9,34 @@ import type { Locale } from '@/types/types';
 import { i18nConfig } from '@/types/types';
 import { useCurrentLocale } from 'next-i18n-router/client';
 
-export const Number: React.FC<
-  {
-    errors: Partial<
-      FieldErrorsImpl<{
-        [x: string]: never;
-      }>
-    >;
-    placeholder?: string;
-    registerAction: UseFormRegister<string & FieldValues>;
-  } & TextField
-> = ({ name, label, registerAction, required: requiredFromProperties, errors, placeholder }) => {
-  // set default values
-  requiredFromProperties ??= false;
+type NumberInputProps = {
+  errors: Partial<FieldErrorsImpl<Record<string, unknown>>>;
+  placeholder?: string;
+  registerAction: UseFormRegister<string & FieldValues>;
+} & TextField;
+
+export const Number: React.FC<NumberInputProps> = ({
+  name,
+  label,
+  registerAction,
+  required: requiredFromProperties = false,
+  errors,
+  placeholder,
+}) => {
   const hasError = errors[name];
   const locale = useCurrentLocale(i18nConfig);
 
-  ///////////////////////////////////////
-  // render error messages in preview mode
-  ///////////////////////////////////////
   const isInPreview =
     typeof globalThis !== 'undefined' && globalThis.location.href.includes('preview=true');
+
   if (typeof label !== 'string') {
     return isInPreview ? <p className="text-sm text-red-800">Label must be a string!</p> : <></>;
   }
+
   if (typeof name !== 'string') {
     return isInPreview ? <p className="text-sm text-red-800">Name must be a string!</p> : <></>;
   }
 
-  ////////////////////////////////////////
-  // render the number input field
-  ////////////////////////////////////////
   return (
     <div className="mb-4">
       <label className="mb-1 block font-['Inter'] text-sm font-medium text-gray-500" htmlFor={name}>
@@ -47,7 +44,9 @@ export const Number: React.FC<
         {requiredFromProperties && <Required />}
       </label>
       <input
-        className={`h-10 w-full rounded-md border-0 bg-green-100 px-4 py-2 font-['Inter'] text-base text-gray-600 shadow-sm ring-1 ring-inset ${hasError ? 'bg-red-50 ring-red-500' : 'ring-transparent'} focus:ring-conveniat-green transition-all duration-200 placeholder:text-gray-400 focus:bg-white focus:ring-2 focus:outline-none focus:ring-inset`}
+        className={`h-10 w-full rounded-md border-0 bg-green-100 px-4 py-2 font-['Inter'] text-base text-gray-600 shadow-sm ring-1 ring-inset ${
+          hasError ? 'bg-red-50 ring-red-500' : 'ring-transparent'
+        } focus:ring-conveniat-green transition-all duration-200 placeholder:text-gray-400 focus:bg-white focus:ring-2 focus:outline-none focus:ring-inset`}
         id={name}
         type="number"
         placeholder={placeholder}
