a0cc7be10fc51883f957d30e12154daa96693e65
Cyrill Püntener <wp99cp@gmx.net>
2025-08-09 19:56:29 +0200

    fix localhost dev server

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,52 +1,46 @@
 import build from '@/build';
 
-import { PrismaInstrumentation } from '@prisma/instrumentation';
 import type { Configuration } from '@vercel/otel';
-import { FetchInstrumentation, OTLPHttpJsonTraceExporter, registerOTel } from '@vercel/otel';
 
 export function register(): void {
-  const baseOTELConfig: Configuration = {
-    serviceName: 'conveniat27-app',
-    traceExporter: new OTLPHttpJsonTraceExporter({
-      url: 'http://tempo:4318/v1/traces',
-    }),
-    attributes: {
-      version: build.version,
-      commitHash: build.git.hash,
-      branch: build.git.branch,
-    },
-    instrumentations: [
-      new FetchInstrumentation({ enabled: true }),
-      new PrismaInstrumentation({ enabled: true }),
-    ],
-  };
-
   // here we cannot use `environmentVariables` because that's only available in the node runtime
   // eslint-disable-next-line n/no-process-env
   if (process.env['NEXT_RUNTIME'] === 'nodejs') {
     const instantiateOTLP = async (): Promise<void> => {
+      // we cannot import these packages at the top level
+      // because they are only available in the node runtime
+      const { FetchInstrumentation, OTLPHttpJsonTraceExporter, registerOTel } = await import(
+        '@vercel/otel'
+      );
       const { OTLPLogExporter } = await import('@opentelemetry/exporter-logs-otlp-http');
       const { logs } = await import('@opentelemetry/sdk-node');
+      const { PrismaInstrumentation } = await import('@prisma/instrumentation');
+
+      const baseOTELConfig: Configuration = {
+        serviceName: 'conveniat27-app',
+        traceExporter: new OTLPHttpJsonTraceExporter({
+          url: 'http://tempo:4318/v1/traces',
+        }),
+        attributes: {
+          version: build.version,
+          commitHash: build.git.hash,
+          branch: build.git.branch,
+        },
+        instrumentations: [
+          new FetchInstrumentation({ enabled: true }),
+          new PrismaInstrumentation({ enabled: true }),
+        ],
+      };
 
       const logRecordProcessor = new logs.BatchLogRecordProcessor(
-        new OTLPLogExporter({
-          url: 'http://loki:3100',
-        }),
+        new OTLPLogExporter({ url: 'http://loki:3100' }),
       );
 
-      registerOTel({
-        ...baseOTELConfig,
-        logRecordProcessor,
-      });
+      registerOTel({ ...baseOTELConfig, logRecordProcessor });
     };
 
     instantiateOTLP().catch((error: unknown) => {
       console.error('Failed to load OTLPLogExporter:', error);
-    });
-  } else {
-    registerOTel({
-      ...baseOTELConfig,
-      // No logRecordProcessor for non-Node.js environments, i.e. the middleware runtime
     });
   }
 }
