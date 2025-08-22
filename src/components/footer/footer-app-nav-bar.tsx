ebdb8a665a79ecb7db189c7663fc6467affbd65f
Cyrill Püntener <wp99cp@gmx.net>
2025-08-22 10:36:15 +0200

    fix footer-width

--- src/components/footer/footer-app-nav-bar.tsx
+++ src/components/footer/footer-app-nav-bar.tsx
@@ -40,7 +40,7 @@ export const FooterAppNavBar: React.FC<{
 
   return (
     <>
-      <div className="fixed bottom-0 z-40 flex h-20 w-full border-t-2 border-gray-200 bg-[#f8fafc]">
+      <div className="fixed bottom-0 z-40 flex h-20 w-full border-t-2 border-gray-200 bg-[#f8fafc] xl:w-[calc(100%-480px)]">
         <nav className="flex w-full items-center justify-around px-4">
           {footerNavBarMenuEntries.map((item) => {
             const isActive = longestMatch === item.href || (isLoading && loadingHref === item.href);
