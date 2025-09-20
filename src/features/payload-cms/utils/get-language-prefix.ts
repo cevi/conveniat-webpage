1aee6c4ea7e1a144f061618ec790433b8782ec72
Cyrill Püntener <wp99cp@gmx.net>
2025-09-20 19:56:51 +0200

    use proper comparison

--- src/features/payload-cms/utils/get-language-prefix.ts
+++ src/features/payload-cms/utils/get-language-prefix.ts
@@ -1,7 +1,7 @@
 import type { Locale } from '@/types/types';
 
 export const getLanguagePrefix = (locale: Locale | undefined): string => {
-  if (locale == undefined) return '';
+  if (locale === undefined) return '';
   // default lang is german
   return locale === 'de' ? '' : `${locale}`;
 };
