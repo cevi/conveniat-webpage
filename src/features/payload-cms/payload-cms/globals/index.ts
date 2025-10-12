7c521f6bbd8b90541a315fbeb1581922d5112d09
maede97 <maede97@hotmail.com>
2025-10-12 12:31:02 +0200

    make linter happy

--- src/features/payload-cms/payload-cms/globals/index.ts
+++ src/features/payload-cms/payload-cms/globals/index.ts
@@ -2,8 +2,8 @@ import { FooterGlobal } from '@/features/payload-cms/payload-cms/globals/footer-
 import { HeaderGlobal } from '@/features/payload-cms/payload-cms/globals/header-global';
 import { PWAGlobal } from '@/features/payload-cms/payload-cms/globals/pwa-global';
 import { SeoGlobal } from '@/features/payload-cms/payload-cms/globals/seo-global';
-import type { GlobalConfig } from 'payload';
 import { SettingsGlobal } from '@/features/payload-cms/payload-cms/globals/settings-global';
+import type { GlobalConfig } from 'payload';
 
 export const globalConfig: GlobalConfig[] = [
   HeaderGlobal,
