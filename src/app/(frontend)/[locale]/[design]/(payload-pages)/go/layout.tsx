3c34273bb5a223a6e593c8f1c3c87c61919d2a04
Cyrill Püntener <wp99cp@gmx.net>
2025-12-22 10:51:58 +0100

    feat: internationalize link resolution for short links

--- src/app/(frontend)/[locale]/[design]/(payload-pages)/go/layout.tsx
+++ src/app/(frontend)/[locale]/[design]/(payload-pages)/go/layout.tsx
@@ -1,9 +1,21 @@
+import type { Locale, StaticTranslationString } from '@/types/types';
 import type React from 'react';
-import type { ReactNode } from 'react';
-import { Suspense } from 'react';
+import { type ReactNode, Suspense } from 'react';
 
-const GoSuspenseWrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <Suspense fallback={<span>Resolve Link Target...</span>}>{children}</Suspense>;
+const resolveLinkTargetText: StaticTranslationString = {
+  de: 'Link-Ziel wird aufgelöst...',
+  en: 'Resolving Link Target...',
+  fr: 'Résolution de la cible du lien...',
+};
+
+interface LayoutProperties {
+  children: ReactNode;
+  params: Promise<{ locale: Locale }>;
+}
+
+const GoSuspenseWrapper: React.FC<LayoutProperties> = async ({ children, params }) => {
+  const { locale } = await params;
+  return <Suspense fallback={<span>{resolveLinkTargetText[locale]}</span>}>{children}</Suspense>;
 };
 
 export default GoSuspenseWrapper;
