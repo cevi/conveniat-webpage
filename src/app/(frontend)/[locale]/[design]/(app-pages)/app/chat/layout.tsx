181df97c1edf4489748c7fc22e6329587f780916
Cyrill Püntener <wp99cp@gmx.net>
2025-12-29 16:29:35 +0100

    fix: remove duplicated ReactQueryDevtools

--- src/app/(frontend)/[locale]/[design]/(app-pages)/app/chat/layout.tsx
+++ src/app/(frontend)/[locale]/[design]/(app-pages)/app/chat/layout.tsx
@@ -1,9 +1,7 @@
 import { SetHideHeader } from '@/components/header/hide-header-context';
 import { SetDynamicPageTitle } from '@/components/header/set-dynamic-app-title';
 import { SetOnlineStatus } from '@/features/chat/components/set-online-status';
-import { TRPCProvider } from '@/trpc/client';
 import type { Locale, StaticTranslationString } from '@/types/types';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { ReactNode } from 'react';
 import React from 'react';
 
@@ -34,13 +32,12 @@ const Layout: React.FC<LayoutProperties> = async ({ children, params }) => {
   }
 
   return (
-    <TRPCProvider>
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
+    <>
       <SetOnlineStatus />
       <SetHideHeader value />
       <SetDynamicPageTitle newTitle={chatsTitle[locale]} />
       {children}
-    </TRPCProvider>
+    </>
   );
 };
 
