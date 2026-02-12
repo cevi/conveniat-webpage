cc015c09e292eea134d8de2cfd27f40add4dcfce
Cyrill Püntener <wp99cp@gmx.net>
2026-02-12 16:42:37 +0100

    fix: DB Index #782

---
+++ prisma/migrations/20260212154229_add_missing_index/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Chat_name_idx" ON "Chat"("name");
+
+-- CreateIndex
+CREATE INDEX "Chat_description_idx" ON "Chat"("description");
+
+-- CreateIndex
+CREATE INDEX "User_name_idx" ON "User"("name");
