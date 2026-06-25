4e70b2a25de9c7556a3c189d8fea9b5b51a74548
Cyrill Püntener <wp99cp@users.noreply.github.com>
2026-06-25 21:57:25 +0200

    fix: resolve typescript error on restoreClient persister type

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -67,7 +67,7 @@ const persister: Persister =
   typeof window === 'undefined'
     ? {
         persistClient: (): Promise<void> => Promise.resolve(),
-        restoreClient: () => Promise.resolve(),
+        restoreClient: (): Promise<undefined> => Promise.resolve(),
         removeClient: (): Promise<void> => Promise.resolve(),
       }
     : createAsyncStoragePersister({
